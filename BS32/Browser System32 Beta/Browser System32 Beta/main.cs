22bff0fd5cc366bb6338dc09681cd4c32b7ea690
NotPetrica <78901126+NotPetrica@users.noreply.github.com>
2021-03-19 12:34:14 +0200

    Add files via upload

---
+++ BS32/Browser System32 Beta/Browser System32 Beta/main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyTabs;
+
+namespace Browser_System32_Beta
+{
+    public partial class Form1 : Form
+    {
+        protected TitleBarTabs ParentTabs
+        {
+            get
+            {
+                return (ParentForm as TitleBarTabs);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("file:///C:/Program%20Files/Browser%20System32/settings.html");
+        }
+    }
+}
